574716c7d566fa51e01cb73aeb860e263bdff48a
vishnugopy <vishnugoby0010@gmail.com>
2023-01-06 16:37:07 +0100

    more changes to improve

--- pages/index.js
+++ pages/index.js
@@ -7,11 +7,11 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Create Next App</title>
+        <title>Cinema Monkeys</title>
         <meta name="description" content="Cinema Monkeys" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link
           href="https://fonts.googleapis.com/css2?family=Arima+Madurai:wght@100;200;300;400;500;700;800;900&display=swap"
           rel="stylesheet"
