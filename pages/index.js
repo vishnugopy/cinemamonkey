7d534d4f30fb47b4298005f92871e2a5e97836aa
vishnugopy <vishnugoby0010@gmail.com>
2023-01-08 00:43:10 +0100

    cleaned and added font

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,36 @@
 import Head from "next/head";
+import Header from "../components/Header/header";
 import styles from "../styles/Home.module.css";
 import Link from "next/link";
-import Header from "../components/Header/header";
 import { useEffect, useState } from "react";
 import { getDocs } from "firebase/firestore";
 import { collection } from "firebase/firestore";
 import { db } from "./api/firebase";
+import { Roboto } from '@next/font/google'
+
+const roboto = Roboto({
+  weight: '400',
+  subsets: ['latin'],
+})
+
 
 export default function Home() {
   const [postsList, setPostsList] = useState([]);
   const postCollectionref = collection(db, "posts");
 
-  
   useEffect(() => {
     const getPosts = async () => {
       const data = await getDocs(postCollectionref);
       setPostsList(
         data.docs.map((doc) => ({
-          ...doc.data(), id:doc.id
+          ...doc.data(),
+          id: doc.id,
         }))
-       )
+      );
     };
 
-    getPosts()
+    getPosts();
   }, []);
-  
-   
-
-   
 
   return (
     <>
@@ -38,13 +41,16 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-      <main className={styles.main}>
-        <div className={styles.grid}>
+      <main className={styles.main} >
+        <div className={styles.container}>
           {postsList.map((post) => {
             return (
-              <Link key={post.id} href={`/${post.id}`} className={styles.card}>
+              <Link className={styles.cards} key={post.id} href={`/${post.id}`} >
                 <h2>{post.title}</h2>
-                <p>{post.content}</p>
+                <div className={styles.author}>
+                  <p>V G</p>
+                  <p>12/01/2023</p>
+                </div>
               </Link>
             );
           })}
