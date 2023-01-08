5724018b46f051206089ad2c516712939a031488
vishnugopy <vishnugoby0010@gmail.com>
2023-01-08 01:46:51 +0100

    added dark mode and some images

--- components/Header/header.js
+++ components/Header/header.js
@@ -3,20 +3,28 @@ import styles from "./header.module.css";
 import Image from "next/image";
 
 export default function Header() {
+    
   return (
     <header className={styles.header}>
         <div className={styles.headerDiv}>
         <Link href={"/"}>
         <Image
-          src="Logo.svg"
-          alt="Picture of the author"
+        className={styles.logo}
+          src="Logo.svg" 
+          alt="Logo of Cinema Monkeys"
           width={80}
           height={60}
         />
       </Link>
 
       <Link href={"/login"}>
-        <div className={styles.loginButton}></div>
+      <Image
+        className={styles.loginButton}
+          src="Profile.svg" 
+          alt="Profile of Cinema Monkeys"
+          width={80}
+          height={60}
+        />
       </Link>
         </div>
      
