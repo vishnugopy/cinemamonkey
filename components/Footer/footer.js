c3b8e8b09230a0f7358d0be71b18bceb46085ccf
vishnugopy <vishnugoby0010@gmail.com>
2023-01-09 23:40:13 +0100

    added some changes

---
+++ components/Footer/footer.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import styles from "./footer.module.css";
+
+export default function Footer() {
+    
+  return (
+    <footer className={styles.footer}>
+       
+     
+    </footer>
+  );
+}
