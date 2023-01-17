016237d7e02c042e055e035e0d85e3b8a6c30fac
vishnugopy <vishnugoby0010@gmail.com>
2023-01-17 14:57:56 +0100

    adding ad sens

--- pages/index.js
+++ pages/index.js
@@ -33,6 +33,8 @@ export default function Home() {
         <meta name="description" content="Cinema Monkeys" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1294077694838733"
+     crossorigin="anonymous"></script>
       </Head>
       <Header />
       <main className={styles.main}>
