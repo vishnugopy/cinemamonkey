e624df8dd4e7efb1beaa26971e6d6ac1374883e1
vishnugopy <vishnugoby0010@gmail.com>
2023-01-17 12:34:51 +0100

    changed favicon and imrpovement in get on news

--- pages/[id].js
+++ pages/[id].js
@@ -1,4 +1,4 @@
-import { useRouter } from 'next/router'
+import { useRouter } from "next/router";
 import styles from "../styles/news.module.css";
 import { db } from "./api/firebase";
 import { doc, getDoc } from "firebase/firestore";
@@ -6,14 +6,25 @@ import { useEffect, useState } from "react";
 
 export default function NewsPage() {
   const [article, setArticle] = useState([]);
-  const router = useRouter()
+  console.log(article);
+  const router = useRouter();
   const { id } = router.query;
-  
+
+  const docRef = doc(db, "posts", id);
+  console.log(docRef);
   useEffect(() => {
     const getAPosts = async () => {
-      const docRef = doc(db, "posts", id);
-      const docSnap = await getDoc(docRef);
-      setArticle(docSnap._document.data.value.mapValue.fields);
+      try {
+        const docSnap = await getDoc(docRef);
+        if (docSnap.exists()) {
+          console.log(docSnap.data());
+          setArticle(docSnap._document.data.value.mapValue.fields);
+        } else {
+          console.log("Document does not exist");
+        }
+      } catch (error) {
+        console.log(error);
+      }
     };
 
     getAPosts();
@@ -22,12 +33,18 @@ export default function NewsPage() {
   return (
     <main className={styles.main}>
       <section className={styles.news}>
-        <h1>
-          {article.title ? article.title.stringValue : ""}
-        </h1>
-        <p>
-        {article.content ? article.content.stringValue : ""}
-        </p>
+        <h1>{article.title ? article.title.stringValue : ""}</h1>
+        <p>{article.content ? article.content.stringValue : ""}</p>
+
+        <div>
+          <h2>Sources</h2>
+          <ul className={styles.links}>
+            {/* {JSON.stringify(article.links)} */}
+            {/* {article.links.arrayValue.values.map((link, index) => {
+              return <li key={index}>{link}</li>;
+            })} */}
+          </ul>
+        </div>
 
         <button onClick={() => router.back()}>Go Back</button>
       </section>
