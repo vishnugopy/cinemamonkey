6a6b4799816bbc68198be87e7ab9ec16a72a1ecc
vishnugopy <vishnugoby0010@gmail.com>
2023-10-17 17:21:52 +0200

    style design

--- components/Header/header.js
+++ components/Header/header.js
@@ -1,22 +1,24 @@
 import Link from "next/link";
 import styles from "./header.module.scss";
-import Image from "next/image";
+import "./logo.svg";
 
 export default function Header() {
   return (
     <header className={styles.header}>
       <div className={styles.headerDiv}>
         <Link href={"/"}>
-          <Image
+          <img
             className={styles.logo}
-            src="logo.svg"
+            src="./logo.svg"
             alt="Logo of Cinema Monkeys"
             width={80}
             height={60}
           />
         </Link>
 
-        <Link href={"/leo/leo"}>Leo Dass Font</Link>
+        <nav className={styles.nav}>
+          <Link href={"/leo/leo"}>Leo Dass Font</Link>
+        </nav>
       </div>
     </header>
   );
--- pages/leo/leo.js
+++ pages/leo/leo.js
@@ -6,39 +6,41 @@ export default function Leo() {
   const [name, setName] = useState("");
 
   return (
-    <main className={styles.main}>
-      <section className={styles.form}>
-        <button
-          className={styles.backbutton}
-          onClick={() => {
-            window.history.back();
-          }}
-        >
-          {" "}
-          Back
-        </button>
-        <h1 className={styles.title}>Input your name here</h1>
-        <input
-          type="text"
-          placeholder="Name"
-          onChange={(e) => {
-            setName(e.target.value);
-          }}
-        />
-        <img src={"/api/leoog?title=" + name} />
-        <button
-          className={styles.button}
-          onClick={() => {
-            const a = document.createElement("a");
-            a.href = "/api/leoog?title=" + name;
-            a.download = "leo.png";
-            a.click();
-          }}
-          download
-        >
-          Download
-        </button>
-      </section>
-    </main>
+    <>
+      <Header />
+      <main className={styles.main}>
+        <section className={styles.form}>
+          <button
+            className={styles.backbutton}
+            onClick={() => {
+              window.history.back();
+            }}
+          >
+            Go Back
+          </button>
+          <h1>Input your name here</h1>
+          <input
+            type="text"
+            placeholder="Name"
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+          />
+          <img src={"/api/leoog?title=" + name} />
+          <button
+            className={styles.button}
+            onClick={() => {
+              const a = document.createElement("a");
+              a.href = "/api/leoog?title=" + name;
+              a.download = "leo.png";
+              a.click();
+            }}
+            download
+          >
+            Download
+          </button>
+        </section>
+      </main>
+    </>
   );
 }
